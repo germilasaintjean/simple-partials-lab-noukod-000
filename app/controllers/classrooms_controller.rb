b38e3dd7ffd4d5a7a0a5c54b875966890c67dd1b
Saint-Jean Germila <germila@noukod.com>
2019-02-21 19:53:10 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,8 +1,7 @@
 class ClassroomsController < ApplicationController
   def show
-    @student = Student.find(params[:id])
-    # @student = @classroom.oldest_student
-    # @classroom = Classroom.find(params[:id])
+    @student = @classroom.oldest_student
+    @classroom = Classroom.find(params[:id])
   end
 
   def index
